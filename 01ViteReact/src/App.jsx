d8865769076360b7aace26185dbc2b2f7d46300f
Chirag Agrawal <chiragagrawal@MacBook-Air.local>
2026-07-07 14:36:50 +0530

    using useState conditionally

--- 01ViteReact/src/App.jsx
+++ 01ViteReact/src/App.jsx
@@ -4,16 +4,20 @@ function App() {
   let [counter, setCounter] = useState(0); // useState Hook
 
   function incVal(){
-    // counter++;
-    // document.querySelector('button').innerHTML = `Increase Value: ${counter}`;
-    // // I have to select all spots wherever counter has been used to reflect them on UI via Vanilla JS. But React has a better way to do this via State Management => useState Hook
-    // console.log(counter);
-
-    setCounter(counter + 1); // setCounter is a function which will update the value of counter and also reflect it on UI wherever it has been used.
+    
+    if(counter <20)
+    {
+      setCounter(counter + 1); 
+    }
+    else setCounter(0); 
   }
 
   let decVal = ()=>{
-    setCounter(counter - 1); // setCounter is a function which will update the value of counter and also reflect it on UI wherever it has been used.
+    if(counter > 0)
+    {
+       setCounter(counter - 1);
+    }
+    else setCounter(20);
   }
 
   return (
